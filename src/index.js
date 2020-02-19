34bbf5f5475729c93b961d2b8c9f748df2234aa3
teomrd <mironidis.gr@gmail.com>
2020-02-19 23:32:40 +0000

    Imported data from the World Digital Library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import timeline_json from "./history.json";
+import eras from './data/eras.json';
+import data1 from './data/data-1.json'
+import data2 from './data/data-2.json'
 import "./static/timeline3/css/timeline.css";
+import "./styles.css";
+
+const timelineDataJson = {
+  "title": {
+    "text": {
+      "headline": "History of the World",
+      "text": "<p>A short timeline of the World history</p>"
+    }
+  },
+  ...eras,
+  events: [
+    ...data1.events,
+    ...data2.events
+  ]
+};
 
 (() => {
-  window.timeline = new TL.Timeline("timeline-embed", timeline_json);
+  window.timeline = new TL.Timeline("timeline", timelineDataJson);
 })();
